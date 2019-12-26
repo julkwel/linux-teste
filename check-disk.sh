80751e55c436603a726a70636b8a0de70ea2a1c0
Jul <julienrajerison5@gmail.com>
2019-12-26 19:38:18 +0300

    Update check-disk.sh

--- check-disk.sh
+++ check-disk.sh
@@ -7,6 +7,6 @@ free_disk=$(df /dev/sda | grep -Eo '[0-9]{4}' | awk '{printf $1}')
 if [[ "$free_disk" -le 100000000  ]]
          then
         ## xmessage system
-        zenity --info --text="Your disk is kely sisa it $free_disk sisa tavela"
+        zenity --info --text="Your disk is full, it $free_disk remaining"
 fi
 exit 0
